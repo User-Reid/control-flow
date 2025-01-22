ab9d50b50ab432b16070f188da7fb00bb55eec3c
Reid Herrera <reidherrerabusiness@gmail.com>
2025-01-22 11:37:15 -0600

    storing if else statements in variables

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,8 @@
+fn even_or_odd(number: i32) {
+    let result: &str = if number % 2 == 0 { "even" } else { "odd" };
+    println!("The number is {result}")
+}
+
 fn main() {
-    let taco: &str = "Shit";
-
-    if true {
-        println!("It is true")
-    }
-
-    if false {
-        println!("It is false")
-    }
-
-    if taco.contains('B') || taco.contains('b') {
-        println!("The word {taco} contains a fuckin letter B or b.")
-    } else {
-      println!("This is some bullshit")
-    }
+    even_or_odd(18)
 }
