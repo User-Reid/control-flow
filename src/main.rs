7b7b850f2f715b8b270572849abd5c2c53d8c00c
Reid Herrera <reidherrerabusiness@gmail.com>
2025-01-22 11:21:37 -0600

    using if, else, and else if statements.

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+fn main() {
+    let taco: &str = "Shit";
+
+    if true {
+        println!("It is true")
+    }
+
+    if false {
+        println!("It is false")
+    }
+
+    if taco.contains('B') || taco.contains('b') {
+        println!("The word {taco} contains a fuckin letter B or b.")
+    } else {
+      println!("This is some bullshit")
+    }
+}
