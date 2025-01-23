8d1797e29489facc8154d13fc1b8ddd8353e9932
Reid Herrera <reidherrerabusiness@gmail.com>
2025-01-22 20:38:29 -0600

    understanding recursion :)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
-fn main() {
-    let mut seconds: i8 = 21;
-
-    while seconds > 0 {
-        if seconds % 2 == 0 {
-            println!("{seconds} seconds (even number), skipping 3 seconds...");
-            seconds -= 3;
-            continue;
-        }
-
-        println!("{seconds} seconds until blastoff..");
-        seconds -= 1;
+fn countdown(seconds: i32) {
+    if seconds == 0 {
+        println!("BLASTOFF!🚀")
+    } else {
+        println!("{seconds} seconds to blastoff..");
+        countdown(seconds - 1);
     }
+}
 
-    println!("BLASTOFF!🚀");
+fn main() {
+    countdown(5);
 }
