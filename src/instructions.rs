b22773e0ee98823c5fa182af064426549b728829
Reid Herrera <reidherrerabusiness@gmail.com>
2025-01-22 20:24:56 -0600

    went over while loops as well as implimenting match statements and if statements within them.

---
+++ src/instructions.rs
@@ -0,0 +1,10 @@
+while seconds > 0 {
+  if seconds % 2 == 0 {
+      println!("{seconds} seconds (even number), skipping 3 seconds...");
+      seconds -= 3;
+      continue;
+  }
+
+  println!("{seconds} seconds until blastoff..");
+  seconds -= 1;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 fn main() {
-    let mut seconds: i8 = 20;
+    let mut seconds: i8 = 21;
 
-    loop {
-        if seconds == 0 {
-            println!("BLASTOFF!🚀");
-            break;
+    while seconds > 0 {
+        if seconds % 2 == 0 {
+            println!("{seconds} seconds (even number), skipping 3 seconds...");
+            seconds -= 3;
+            continue;
         }
 
         println!("{seconds} seconds until blastoff..");
         seconds -= 1;
     }
+
+    println!("BLASTOFF!🚀");
 }
